0cc06ac033f155578c94c6f14ddc282dddb7471f
zia <ivanzagoskin55@gmail.com>
2017-11-18 16:06:14 +0300

    changed_form_for_tests

--- DiffusionOfSlowingNeutrons/TestsWindow.xaml.cs
+++ DiffusionOfSlowingNeutrons/TestsWindow.xaml.cs
@@ -19,9 +19,27 @@ namespace NuclearProject
     /// </summary>
     public partial class TestsWindow : Window
     {
+        List<string> lThemes;
+
         public TestsWindow()
         {
             InitializeComponent();
+
+            lThemes = new List<string>();
+            lThemes.Add("Коэффициент размножения");
+            lThemes.Add("Критичность");
+            lThemes.Add("Состав ЯР");
+            lThemes.Add("Деление");
+
+            foreach(string itemThemes in lThemes)
+            {
+                cmbThemes.Items.Add(itemThemes);
+            }
+        }
+
+        private void cmbThemes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
